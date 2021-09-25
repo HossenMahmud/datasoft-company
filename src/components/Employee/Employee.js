1e8cb167b98be54628002dcc238ed9f076619707
HossenMahmud <hossenmahmud15@gmail.com>
2021-09-26 01:59:59 +0600

    updated Design part

--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import './Employee.css';
 
 const Employee = (props) => {
     //console.log(props.employee);
     const { name, age, image, salary, job, gender } = props.employee;
+    const element = <FontAwesomeIcon icon={faUser} />
     return (
         <div className='employee-item'>
             <div className='employee-image'>
@@ -14,7 +17,7 @@ const Employee = (props) => {
             <h5>Age: {age}</h5>
             <h5>Gender: {gender}</h5>
             <h5>Salary: ${salary}</h5>
-            <button className='add-btn'>Add To List</button>
+            <button className='add-btn'>{element}  Add To List</button>
         </div>
     );
 };
